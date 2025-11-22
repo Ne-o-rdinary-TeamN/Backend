4b30b37a0065c7c11718535b9d6c69ef346d0f66
subin930 <bread0930@g.skku.edu>
2025-11-23 04:50:59 +0900

    [feat] 핫 뉴스 전체 목록 조회 구현

--- src/main/java/spring/hackerthon/news/controller/NewsController.java
+++ src/main/java/spring/hackerthon/news/controller/NewsController.java
@@ -1,4 +1,24 @@
 package spring.hackerthon.news.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.hackerthon.news.dto.HotNewsRes;
+import spring.hackerthon.news.service.NewsService;
+
+@Tag(name = "뉴스 컨트롤러")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/news")
 public class NewsController {
+    private final NewsService newsService;
+
+    @GetMapping("/hot")
+    @Operation(summary = "핫 뉴스 전체 목록 조회", description = "핫 뉴스 전체 목록 조회(5개)")
+    public HotNewsRes getHotNews() {
+        return newsService.getHotNews();
+    }
 }
--- src/main/java/spring/hackerthon/news/service/NewsService.java
+++ src/main/java/spring/hackerthon/news/service/NewsService.java
@@ -1,4 +1,30 @@
 package spring.hackerthon.news.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import spring.hackerthon.news.domain.News;
+import spring.hackerthon.news.dto.HotNews;
+import spring.hackerthon.news.dto.HotNewsRes;
+import spring.hackerthon.news.repository.NewsRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class NewsService {
+    private final NewsRepository newsRepository;
+
+    public HotNewsRes getHotNews() {
+        List<News> news = newsRepository.findTop5ByOrderByNewsPkDesc();
+        List<HotNews> hotNews = new ArrayList<>();
+
+        for(News n : news){
+            hotNews.add(HotNews.builder().newsPk(n.getNewsPk()).title(n.getNewsTitle()).url(n.getUrl()).build());
+        }
+
+        return HotNewsRes.builder().newsList(hotNews).build();
+    }
 }
