b909080ee4975332a333cd5a1a55b27ec9ed93ad
subin930 <bread0930@g.skku.edu>
2025-11-23 08:04:05 +0900

    [fix] 충돌 해결

--- src/main/java/spring/hackerthon/post/converter/PostConverter.java
+++ src/main/java/spring/hackerthon/post/converter/PostConverter.java
@@ -9,6 +9,24 @@ import java.util.List;
 
 public class PostConverter {
 
+    public static PostResponseDTO.SinglePostViewResultDTO toSinglePostViewResultDTO(Post post) {
+        return PostResponseDTO.SinglePostViewResultDTO.builder()
+                .postName(post.getTitle())
+                .hashtags(post.getHashtags().stream()
+                        .map(Hashtag::getName).toList())
+                .agreeCount(post.getAgreeCount())
+                .disagreeCount(post.getDisagreeCount())
+                .agreeRate(post.getAgreeRate())
+                .disagreeRate(post.getDisagreeRate())
+                .build();
+    }
+
+    public static PostResponseDTO.TotalPostViewResultDTO toTotalPostViewResultDTO(List<PostResponseDTO.SinglePostViewResultDTO> singlePostViewResultDTOList) {
+        return PostResponseDTO.TotalPostViewResultDTO.builder()
+                .singlePostViewResultDTOList(singlePostViewResultDTOList)
+                .build();
+    }
+
     public static PostResponseDTO.PostCreateResponseDTO toPostCreateResponseDTO(Post post) {
         return PostResponseDTO.PostCreateResponseDTO.builder()
                 .postPk(post.getPostPk())
