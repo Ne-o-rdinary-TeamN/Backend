2d2fed5eae5b107aecb996655a9eb4373fd906ce
subin930 <bread0930@g.skku.edu>
2025-11-23 06:00:10 +0900

    [feat] 투표 기능 구현

--- src/main/java/spring/hackerthon/post/domain/Post.java
+++ src/main/java/spring/hackerthon/post/domain/Post.java
@@ -2,6 +2,7 @@ package spring.hackerthon.post.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import spring.hackerthon.opinion.domain.OpinionType;
 import spring.hackerthon.recommendation.domain.Recommendation;
 import spring.hackerthon.user.domain.User;
 import spring.hackerthon.global.common.BaseEntity;
@@ -55,5 +56,22 @@ public class Post extends BaseEntity {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<Hashtag> hashtags = new ArrayList<>();
+
+    public void updatePost(OpinionType type) {
+        //투표 수 수정
+        if(type.equals(OpinionType.AGREE)) {
+            //동의인 경우
+            this.agreeCount = this.agreeCount + 1;
+        } else {
+            this.disagreeCount = this.disagreeCount + 1;
+        }
+
+        this.totalCount = this.totalCount + 1;
+
+        //비율 수정
+        double agreeRateRaw = ((double) this.agreeCount / this.totalCount) * 100;
+        this.agreeRate = Math.round(agreeRateRaw * 10.0) / 10.0; // 소수점 첫째 자리 반올림
+        this.disagreeRate = 100.0 - this.agreeRate;
+    }
 }
 
